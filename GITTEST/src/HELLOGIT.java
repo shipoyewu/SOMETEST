fae7dd81cbedb98593d1024fe92693e7a3f98221
Yolen Stark <yolenstark@outlook.com>
2015-07-24 11:32:10 +0800

    add something

---
+++ GITTEST/src/HELLOGIT.java
@@ -0,0 +1,15 @@
+import static java.lang.System.*;
+
+/**
+ * Created by zongzan on 2015/7/24.
+ */
+public class HELLOGIT {
+    public static void main(String[] args) {
+        out.println("this is a test");
+        out.printf("add something");
+        out.printf("add something");
+        out.printf("add something");
+        out.printf("add something");
+        out.printf("add something");
+    }
+}
